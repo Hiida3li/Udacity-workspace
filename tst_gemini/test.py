e27f06309f1525ee19911162d1c9733062f9e97c
Hewida Ali <112735381+Hiida3li@users.noreply.github.com>
2025-07-01 13:40:34 +0400

    .

--- tst_gemini/test.py
+++ tst_gemini/test.py
@@ -2,7 +2,7 @@ def connect_to_milvus():
     """Connect to Milvus database."""
 
     milvus_uri = "MILVUS_URI"
-    token = ""
+    token = "MILVUS_TOKENS"
 
     milvus_client = MilvusClient(uri=milvus_uri, token=token)
 
