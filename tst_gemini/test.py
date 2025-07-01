5fddd6ba3d67977e820c592da7642b433033b86f
Hewida Ali <112735381+Hiida3li@users.noreply.github.com>
2025-07-01 13:42:08 +0400

    .

--- tst_gemini/test.py
+++ tst_gemini/test.py
@@ -1,8 +1,17 @@
-def connect_to_milvus():
-    """Connect to Milvus database."""
+import os
+from dotenv import load_dotenv
+from pymilvus import MilvusClient
 
-    milvus_uri = "MILVUS_URI"
-    token = "MILVUS_TOKENS"
+load_dotenv()
+
+def connect_to_milvus():
+    """Connect to Milvus database """
+
+    milvus_uri = os.getenv("MILVUS_URI")
+    token = os.getenv("MILVUS_TOKENS")
+
+    if not milvus_uri or not token:
+        raise ValueError("MILVUS_URI or MILVUS_TOKENS not set in .env")
 
     milvus_client = MilvusClient(uri=milvus_uri, token=token)
 
